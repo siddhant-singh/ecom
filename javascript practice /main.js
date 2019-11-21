e686298338cac5e913310035c3d866b5d0831197
Siddhant Singh <siddhant.singh@icicletech.com>
2019-11-21 12:09:25 +0530

    playing with variables

---
+++ javascript practice /main.js
@@ -0,0 +1,28 @@
+// alert('Hello world');
+// console.warn('this is a warning');
+
+
+// var, let, const
+
+// var is globally defined
+
+// let can reassigned the value. That's why it is diffrent from const.
+
+// const name = 'John';
+// const age = 30;
+// const rating = 4.5;
+// const isCool = true;
+// const x = null;
+// const y = undefined;
+// let z;
+
+// console.log(typeof name);
+
+
+// const name = 'siddhant';
+// const age = '24';
+
+// Concatenation 
+// console.log('My name is ' + name + ' and I am ' + age);
+// Template string
+// console.log(`My name is ${name} and I am ${age}`);
